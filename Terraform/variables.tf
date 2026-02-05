2edff530a676e06bf58c3e3f22b16580e8f66310
Pratik Muthal <pratikmuthal171@gmail.com>
2026-02-05 23:55:10 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,17 @@
+
+variable "instance_name" {
+    default = "Monitoring_server"  
+}
+
+variable "key_name" {
+  default = "terra"                
+}
+
+
+variable "access_key" {
+  default = "insert"                # aws access key
+}
+
+variable "secret_key" {
+  default = "insert"         # aws secret key 
+}
